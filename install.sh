4f6a418aae09beb09c8d775b36deb091f31e377d
ma v <dr98mav@gmail.com>
2020-08-22 22:02:11 +0430

    change somethings and update install for os

--- install.sh
+++ install.sh
@@ -8,14 +8,27 @@ echo "..........................................................."
 echo "....install.python3.and.make.virtualenv.for.run.command...."
 echo "..........................................................."
 
-apt-get update &> /dev/null
-apt-get upgrade &> /dev/null
-apt-get install python3 python3-pip -y &> /dev/null
-python3 -m pip install pipenv &> /dev/null
-python3 -m pipenv check &> /dev/null
-python3 -m pipenv lock &> /dev/null
-python3 -m pipenv update &> /dev/null
-pythone -m pipenv install -r req.txt &> /dev/null
-clear
 
-python3 main.py
+UNAME=$(uname)
+
+if [ "$UNAME" == "Linux" ] ; then
+  apt-get update &> /dev/null
+  apt-get upgrade &> /dev/null
+  apt-get install python3 python3-pip -y &> /dev/null
+  python3 -m pip install pipenv &> /dev/null
+  python3 -m pipenv check &> /dev/null
+  python3 -m pipenv lock &> /dev/null
+  python3 -m pipenv update &> /dev/null
+  pythone -m pipenv install -r req.txt &> /dev/null
+  clear
+
+  python3 main.py
+elif [ "$UNAME" == "Darwin" ] ; then
+	echo "Darwin"
+	echo "not supported"
+	exit
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	echo "Windows"
+	echo "not supported"
+	exit
+fi
