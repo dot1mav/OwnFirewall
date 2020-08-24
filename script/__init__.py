ded990cda93d49c4911f6334e34756474d8da08c
ma v <dr98mav@gmail.com>
2020-08-24 20:47:31 +0430

    update scripts class

--- script/__init__.py
+++ script/__init__.py
@@ -1,27 +1,27 @@
-import os, time
+import os
+import time
 
 
 class Script(object):
-    __os: str = None
 
     def __init__(self):
         self.__welcome()
         time.sleep(2)
-        print('1.config Srv\n'
-              '2.config Client')
-        check: int = int(input('#'))
-        if check == 1:
-            os.system(f'clear')
-            os.system(f'bash ConfigSRV.sh')
-        elif check == 2:
-            os.system(f'clear')
-            os.system(f'bash ConfigClient.sh')
-        else:
-            print(f'wrong number')
-            exit(0)
 
     def __call__(self, *args, **kwargs):
         pass
+
+    def make_service(self):
+        os.system(f'bash MakeStartupFiles.sh')
+
+    def config_srv(self):
+        os.system(f'bash ConfigSRV.sh')
+
+    def config_clt(self):
+        os.system(f'bash ConfigClient.sh')
+
+    def install_def_ps(self):
+        os.system(f'bash PortScanner.sh')
 
     def __welcome(self):
         print("...........................................\n\
@@ -36,5 +36,5 @@ class Script(object):
     ...........................................\n\
     ...........................................")
 
-    def make_service(self):
-        os.system(f'bash MakeStartupFiles.sh')
+
+
