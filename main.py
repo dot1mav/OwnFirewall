5e3607ecc7bb02a86858bdc2d26d2a07dee3f60f
ma v <dr98mav@gmail.com>
2020-08-26 21:19:53 +0430

    update main file

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ if __name__ == '__main__':
             break
         else:
             print('choose wrong number\n')
+
+    print('make startup this feature')
+    st.make_service()
+    print('install port scanner blocker(y/n)(default=y)')
+    temp: str = input('> ')
+    if temp == 'y' or temp is None:
+        st.install_def_ps()
+    print('install fail2ban(y/n)(default=y)')
+    temp: str = input('> ')
+    if temp == 'y' or temp is None:
+        st.install_fail2ban()
